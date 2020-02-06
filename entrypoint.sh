20040caff343ac8fdb3553efc914ddcf1cec482f
James Webb <james.webb@bristol.ac.uk>
2020-02-06 16:02:57 +0000

    Update parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ challenge=`ssh-keyscan -t rsa github.com 2>/dev/null | ssh-keygen -lf -`
 if [ "$challenge" = "$sig" ]; then
     mkdir /root/.ssh/
     chmod 700 /root/.ssh/
-    echo $1 > /root/.ssh/deploy.key
-    chmod 600 /root/.ssh/deploy.key
-    export GIT_SSH_COMMAND="ssh -i /root/.ssh/deploy.key"
+    echo $INPUT_SSH_KEY > /root/.ssh/ssh.key
+    chmod 600 /root/.ssh/ssh.key
+    export GIT_SSH_COMMAND="ssh -i /root/.ssh/ssh.key"
     ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
     # Checkout the submodules!
     git submodule update --init --recursive
