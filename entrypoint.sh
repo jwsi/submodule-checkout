f0c559c913b311da5d11a5dc0601206df1b9cdc0
James Webb <james.webb@bristol.ac.uk>
2020-02-06 15:45:15 +0000

    Update system to allow for deploy keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -l
 
 sig=`cat /github.sig`
-challenge=`ssh-keyscan -t rsa github.com | ssh-keygen -lf -`
+challenge=`ssh-keyscan -t rsa github.com 2>/dev/null | ssh-keygen -lf -`
 
 if [ "$challenge" = "$sig" ]; then
     mkdir /root/.ssh/
-    chmod 0700 /root/.ssh/
-    ssh-keyscan github.com >> /root/.ssh/known_hosts
+    chmod 700 /root/.ssh/
+    echo $1 > /root/.ssh/deploy.key
+    chmod 600 /root/.ssh/deploy.key
+    export GIT_SSH_COMMAND="ssh -i /roo/.ssh/deploy.key
+    ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
     # Checkout the submodules!
     git submodule update --init --recursive
 else
