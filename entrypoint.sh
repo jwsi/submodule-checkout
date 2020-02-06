a38b3fb518a5117c088997f531653261a9927fc5
James Webb <james.webb@bristol.ac.uk>
2020-02-06 15:04:23 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
-sig=`cat ./github.sh`
+sig=`cat ./github.sig`
 challenge=`ssh-keyscan -t rsa github.com | ssh-keygen -lf -`
 
-if [ "$challenge" = "$sig"]
+if [ "$challenge" = "$sig" ]; then
     ssh-keyscan github.com >> ~/.ssh/known_hosts
     # Checkout the submodules!
     git submodule update --init --recursive
 else
     echo "Signature validation failed!"
-    exit(1)
+    exit 1
 fi;
