53bc90962b3786268a56e913e98337ac4462d050
James Webb <james.webb@bristol.ac.uk>
2020-02-06 15:11:12 +0000

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ sig=`cat /github.sig`
 challenge=`ssh-keyscan -t rsa github.com | ssh-keygen -lf -`
 
 if [ "$challenge" = "$sig" ]; then
-    mkdir ~/.ssh/
-    chmod 0700 ~/.ssh/
-    ssh-keyscan github.com >> ~/.ssh/known_hosts
+    mkdir /root/.ssh/
+    chmod 0700 /root/.ssh/
+    ssh-keyscan github.com >> /root/.ssh/known_hosts
     # Checkout the submodules!
     git submodule update --init --recursive
 else
