4c47af3b214733c72291c10b43727abce8cd4143
James Webb <james.webb@bristol.ac.uk>
2020-02-06 15:55:31 +0000

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$challenge" = "$sig" ]; then
     chmod 700 /root/.ssh/
     echo $1 > /root/.ssh/deploy.key
     chmod 600 /root/.ssh/deploy.key
-    export GIT_SSH_COMMAND="ssh -i /roo/.ssh/deploy.key
+    export GIT_SSH_COMMAND="ssh -i /root/.ssh/deploy.key"
     ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
     # Checkout the submodules!
     git submodule update --init --recursive
