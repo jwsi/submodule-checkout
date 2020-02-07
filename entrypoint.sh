2ba9b2b59a1f39b65ee415b1925c542432f5adec
James Webb <james@ultra-horizon.com>
2020-02-07 12:47:38 +0000

    Update key writing system

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,10 @@
 sig=`cat /github.sig`
 challenge=`ssh-keyscan -t rsa github.com 2>/dev/null | ssh-keygen -lf -`
 
-echo "this is a test!"
-
 if [ "$challenge" = "$sig" ]; then
     mkdir /root/.ssh/
     chmod 700 /root/.ssh/
-    echo $1 > /root/.ssh/ssh.key
-    cat /root/.ssh/ssh.key
+    echo "$1" > /root/.ssh/ssh.key
     chmod 600 /root/.ssh/ssh.key
     export GIT_SSH_COMMAND="ssh -i /root/.ssh/ssh.key"
     ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
