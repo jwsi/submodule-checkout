423a9fa739d85113c47e9253bb0172f55d8f48ac
James Webb <james@ultra-horizon.com>
2020-02-07 12:38:34 +0000

    Test again

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ if [ "$challenge" = "$sig" ]; then
     mkdir /root/.ssh/
     chmod 700 /root/.ssh/
     echo $1 > /root/.ssh/ssh.key
-    echo /root/.ssh/ssh.key
+    cat /root/.ssh/ssh.key
     chmod 600 /root/.ssh/ssh.key
     export GIT_SSH_COMMAND="ssh -i /root/.ssh/ssh.key"
     ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
