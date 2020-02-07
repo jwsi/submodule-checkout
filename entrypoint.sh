428791ee676835d4c6cb9998b38977c5c89fba46
James Webb <james@ultra-horizon.com>
2020-02-07 13:27:22 +0000

    Make the .ssh directory within alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ sig=`cat /github.sig`
 challenge=`ssh-keyscan -t rsa github.com 2>/dev/null | ssh-keygen -lf -`
 
 if [ "$challenge" = "$sig" ]; then
+    mkdir /root/.ssh/
+    chmod 700 /root/.ssh/
     ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
     if [ "$1" = "" ]; then
         echo "No SSH key detected, attempting public checkout"
         git submodule update --init --recursive
     else
         echo "SSH key detected, attempting private checkout"
-        mkdir /root/.ssh/
-        chmod 700 /root/.ssh/
         echo "$1" > /root/.ssh/ssh.key
         chmod 600 /root/.ssh/ssh.key
         export GIT_SSH_COMMAND="ssh -i /root/.ssh/ssh.key"
